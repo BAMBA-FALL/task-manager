98748f01bbe65f6028d0cbeb186b4238ba337d7c
BAMBA-FALL <khadimmbackefall1@gmail.com>
2024-12-01 11:09:00 +0100

    Premier commit

---
+++ app/src/main/java/com/example/taskmanager/AlarmReceiver.kt
@@ -0,0 +1,10 @@
+package com.example.taskmanager
+
+import android.content.BroadcastReceiver
+
+class AlarmReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        val taskName = intent.getStringExtra("task_name")
+        Toast.makeText(context, "C'est l'heure de votre tâche: $taskName!", Toast.LENGTH_LONG).show()
+    }
+}
